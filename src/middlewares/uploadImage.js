b6086c35e22bf5b6cf3a4e4e8cc9f8069161a366
alejandroch1202 <alejandrochavez1202@gmail.com>
2023-11-22 17:22:51 -0400

    Fixing uploads path for deploy and adding test img

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -1,10 +1,13 @@
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import multer from 'multer'
-import path from 'path'
 import { generateId } from './../helpers/token.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/uploads')
+    cb(null, path.join(__dirname, './../../public/uploads'))
   },
   filename: (req, file, cb) => {
     cb(null, generateId() + path.extname(file.originalname))
